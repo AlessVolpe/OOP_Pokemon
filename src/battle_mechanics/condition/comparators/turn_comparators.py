29c983444cc46a1c8ca319e2b16b668be93c1609
Alessandro Volpe <37452470+AlessVolpe@users.noreply.github.com>
2026-05-25 21:27:18 +0200

    feat: implement turn comparators

---
+++ src/battle_mechanics/condition/comparators/turn_comparators.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from battle_mechanics.target import TargetInterface
+from battle_mechanics.condition.condition_interface import ConditionInterface
+
+
+class BattleCondition[Turn](ConditionInterface):
+    turn: Turn
+
+    def __init__(self, turn: Turn) -> None:
+        self.turn = turn
+
+    def check(self) -> bool:
+        pass
+
+
+class ForLifter(BattleCondition):
+    target: TargetInterface
+
+    def __init__(self, target: TargetInterface, /, **data: Any) -> None:
+        super().__init__(**data)
+        self.target = target
