32b7b4b321c07620d407f7b3fe0b723ffda531d8
Alessandro Volpe <37452470+AlessVolpe@users.noreply.github.com>
2026-05-25 21:27:10 +0200

    feat: implement generic comparators

---
+++ src/battle_mechanics/condition/comparators/generic_comparators.py
@@ -0,0 +1,41 @@
+from battle_mechanics.condition.condition_interface import ConditionInterface, T
+
+
+class BothCondition[T](ConditionInterface):
+    condition: T
+
+    def __init__(self, condition: T) -> None:
+        self.condition = condition
+
+    def check(self) -> bool:
+        return self.condition[0] and self.condition[1]
+
+
+class EitherCondition[T](ConditionInterface):
+    condition: T
+
+    def __init__(self, condition: T) -> None:
+        self.condition = condition
+
+    def check(self) -> bool:
+        return self.condition[0] or self.condition[1]
+
+
+class NotCondition[T](ConditionInterface):
+    condition: T
+
+    def __init__(self, condition: T) -> None:
+        self.condition = condition
+
+    def check(self) -> bool:
+        return not self.condition
+
+
+class Chance[float](ConditionInterface):
+    condition: float
+
+    def __init__(self, condition: float) -> None:
+        self.condition = condition
+
+    def check(self) -> bool:
+        pass
